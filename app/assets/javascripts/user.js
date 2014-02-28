d30d0da57676d3b9dd8632044b49eecfc1c73897
Peter Hrbacik <hrbacik@gmail.com>
2014-02-28 17:13:30 +0100

    School auto-complete

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -24,3 +24,30 @@ function SubmitSignupForm () {
     }
 
 }
+
+function searchSchool(query) {
+    if (query == '') {
+        $('#schools').html('');
+        return;
+    }
+
+    $.ajax({
+        url: 'http://backend.liquidtalent.com/schools/search?search=' + query,
+        error: function (error) {
+            alert("Could not connect to the mail server. Please try again later.");
+            console.log(error);
+        }
+    }).done(function (data) {
+        $('#schools').html('');
+            data['response'].forEach(function(school) {
+            $('#schools').append('<div onclick="setSchool(' + school['_source']['id'] + ', \'' + school['_source']['name'] + '\');">' + school['_source']['name'] + '</div>')
+        })
+    });
+}
+
+function setSchool(id, name) {
+    $('.school').html(name);
+    $('#school_id').val(id);
+
+    $('#schools').html('');
+}
