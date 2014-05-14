abb1f63b88b3b39258e6742c862253087dea00ab
Oscar Guinane <Oscar@bloodandtreasure.com>
2014-05-14 03:12:00 +0100

    Adding IPA file to the website.

--- app/controllers/presentation_controller.rb
+++ app/controllers/presentation_controller.rb
@@ -55,7 +55,7 @@ class PresentationController < ApplicationController
   end
 
   def app
-   render 'presentation/app.plist', :layout => 'application'
+   render 'presentation/app', :layout => 'application'
   end
   def ipa_downloader
    render 'presentation/ipa_downloader.plist', :layout => 'application'
